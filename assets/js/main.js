d3cba0298c96231949aca5c9638e27e1aaed1f1d
Raul Dionisio <raul.dionisio@cortex-intelligence.com>
2023-08-31 18:35:48 -0300

    Adicionando tipos dinâmicamente e adicionando paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,32 @@
 
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-    <li class="pokemon" class=${pokemon.type}>
-    <span class="number">${pokemon.number}</span>
-    <span class="name">${pokemon.name}</span>
+function loadPokemonItens(offset, limit) {
+    pokeapi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>
+            `<li class="pokemon ${pokemon.type}">
+        <span class="number">${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
 
-    <div class="detail">
-        <ol class="types"> 
-         ${pokemon.types.map((type) => `<li class="type">${type}</l1>`).join('')}
-        </ol>
-        <img src=${pokemon.photo} 
-            alt="${pokemon.name}" srcset="">
-    </div>
-    </li>
-    `
+        <div class="detail">
+            <ol class="types"> 
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</l1>`).join('')}
+            </ol>
+            <img src=${pokemon.photo} 
+                alt="${pokemon.name}" srcset="">
+        </div>
+        </li>
+    `).join("")
+        pokemonList.innerHTML += newHtml;
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit);
 
-pokeapi.getPokemons().then((pokemons = []) => {
-
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("")
-
+loadMoreButton.addEventListener("click", () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
